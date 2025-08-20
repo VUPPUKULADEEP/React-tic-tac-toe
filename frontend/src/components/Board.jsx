20eb454807ce2c7110680317c91029b364ef43cb
VUPPU KULADEEP <vuppukuladeep@gmail.com>
2025-08-20 21:44:08 +0530

    removed unwanted code

---
+++ frontend/src/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div>
+      <p>hi</p>
+    </div>
+  )
+}
+
+export default Board
---
+++ frontend/src/pages/Tictactoe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Board from '../components/Board'
+import Heading from '../components/Heading'
+
+
+const Tictactoe = () => {
+  return (
+    <div>
+      <Heading/>
+      <Board/>
+    </div>
+  )
+}
+
+export default Tictactoe
